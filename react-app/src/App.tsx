9c7279c024c1fefa1d4a5500a2e484dab689f446
kng03 <longkhoa1994@gmail.com>
2024-12-01 10:47:40 +0700

    Adding library
    https://www.youtube.com/watch?v=SdphnMywCbo&list=PL_-VfJajZj0UXjlKfBwFX73usByw3Ph9Q&index=10

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,24 @@
 import Message from "./components/ConditionWithSimpleMessage";
 import ListGroup from "./components/ShowListGroup";
 import loggerArrowSynctax from "./components/ArrowFunctionSyntax";
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+
+function HeaderDemoInSideFileApp()
+{
+  return (
+    <>
+      <h1> Header </h1>
+      <img src="/react.svg" alt="React Logo"/>
+    </> 
+    )
+}
+
 function App() {
   return (
     <>
-      <h1> App </h1>
-      <Message></Message>
-      <Message />   <Message />
-      {loggerArrowSynctax('abc')}
+      <HeaderDemoInSideFileApp/>
+      
     </>
   );
 }
